f24c7cbf7532528e735eaeafe11e67c741feeba7
Leo <leononame@lho.io>
2018-05-03 20:25:46 +0200

    Add group with gid 1000 and add user leo to group

--- post-install.sh
+++ post-install.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 if [[ $EUID -ne 0 ]]; then
-  echo "You must be a root user" 2>&1
+  echo "You must be root" 2>&1
   exit 1
 fi
 
 # Source variables
 source vars.sh
 
+# Add user and group
+echo "Create group leo and add user leo..."
+groupadd -g 1000 leo
+usermod -a -G leo leo
+
 # Install stuff that's needed before doing anything
-echo "Installing rsync"
+echo "Installing rsync..."
 $inst rsync
 
 source backup/data.sh
