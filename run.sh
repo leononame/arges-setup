a4c9762dc624476787776dca1bfdc0bc6fb547fd
Leo <leononame@lho.io>
2018-05-04 14:05:53 +0200

    Add terminal profile

--- run.sh
+++ run.sh
@@ -23,7 +23,7 @@ dconf write /system/locale/region "'es_ES.UTF-8'"
 
 # Extension settings
 # Install extensions
-aurman --needed --noconfirm --noedit -S gnome-shell-extension-no-title-bar-git gnome-shell-extension-audio-output-switcher-git gnome-shell-extension-topicons-plus-git gnome-shell-extension-workspace-grid-git gnome-shell-extension-caffeine-git
+aurman --needed --noconfirm --noedit -S gnome-shell-extension-no-title-bar-git gnome-shell-extension-audio-output-switcher-git gnome-shell-extension-topicons-plus-git gnome-shell-extension-workspace-grid-git 
 # Restart gnome-shell
 
 #Enable extensions
@@ -48,3 +48,23 @@ dconf write /org/gnome/shell/overrides/dynamic-workspaces "false"
 dconf write /org/gnome/shell/extensions/workspace-grid/num-columns "2"
 dconf write /org/gnome/shell/extensions/workspace-grid/num-rows "2"
 dconf write /org/gnome/shell/extensions/workspace-grid/show-workspace-labels "false"
+
+# Terminal settings
+# Hide menubar
+dconf write /org/gnome/terminal/legacy/default-show-menubar "false"
+# Profiles
+dconf write /org/gnome/terminal/legacy/profiles:/default "'b1dcc9dd-5262-4d8d-a863-c897e6d979b9'"
+dconf write /org/gnome/terminal/legacy/profiles:/list "[b1dcc9dd-5262-4d8d-a863-c897e6d979b9]"
+profile="/org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9"
+dconf write $profile/audible-bell "false"
+dconf write $profile/background-color "rgb(0,0,0)"
+dconf write $profile/default-size-columns "120"
+dconf write $profile/foreground-color "rgb(78,154,6)"
+dconf write $profile/scrollbar-policy "'never'"
+dconf write $profile/use-theme-colors "false"
+dconf write $profile/visible-name "'Leo'"
+dconf write $profile/palette "['#2E3436', '#CC0000', '#4E9A06', '#C4A000', '#3465A4', '#75507B', '#06989A', '#D3D7CF', '#555753', '#EF2929', '#8AE234', '#FCE94F', '#729FCF', '#AD7FA8', '#34E2E2', '#EEEEEC']"
+# Nautilus settings
+dconf write /org/gnome/nautilus/list-view/default-column-order "['name', 'size', 'type', 'owner', 'group', 'permissions', 'mime_type', 'where', 'date_modified', 'date_modified_with_time', 'date_accessed', 'recency', 'starred']"
+dconf write /org/gnome/nautilus/list-view/default-visible-columns "['name', 'size', 'type', 'owner', 'group', 'permissions', 'date_modified']"
+dconf write /org/gnome/nautilus/preferences/default-folder-viewer "'list-view'"
