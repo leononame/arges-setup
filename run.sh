6c85ea53ac948a5f48c872ffcaba9c9d20e0c19b
Leo <leononame@lho.io>
2018-05-04 13:28:46 +0200

    Add some extension stuff

--- run.sh
+++ run.sh
@@ -22,10 +22,29 @@ dconf write /org/gnome/desktop/input-sources/sources "[('xkb', 'us'), ('xkb', 'd
 dconf write /system/locale/region "'es_ES.UTF-8'"
 
 # Extension settings
+# Install extensions
+aurman --needed --noconfirm --noedit -S gnome-shell-extension-no-title-bar-git gnome-shell-extension-audio-output-switcher-git gnome-shell-extension-topicons-plus-git gnome-shell-extension-workspace-grid-git gnome-shell-extension-caffeine-git
+# Restart gnome-shell
+
 #Enable extensions
 #dconf write /org/gnome/shell/enabled-extensions ''
+
+# Settings
 # Dash to Dock
 dconf write /org/gnome/shell/extensions/dash-to-dock/extend-height 'true'
 dconf write /org/gnome/shell/extensions/dash-to-dock/custom-theme-shrink 'true'
 dconf write /org/gnome/shell/extensions/dash-to-dock/intellihide 'false'
 
+# No title bar
+dconf write /org/gnome/shell/extensions/no-title-bar/button-position "'before-status-area'"
+dconf write /org/gnome/shell/extensions/no-title-bar/change-appmenu "false"
+
+# TopIcons
+dconf write /org/gnome/shell/extensions/topicons/tray-pos "'right'"
+dconf write /org/gnome/shell/extensions/topicons/tray-order "2"
+
+# Workspace grid
+dconf write /org/gnome/shell/overrides/dynamic-workspaces "false"
+dconf write /org/gnome/shell/extensions/workspace-grid/num-columns "2"
+dconf write /org/gnome/shell/extensions/workspace-grid/num-rows "2"
+dconf write /org/gnome/shell/extensions/workspace-grid/show-workspace-labels "false"
