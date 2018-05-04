0b3215724ee6b31c52f3e74876b95a8bf4ef8f61
Leo <leononame@lho.io>
2018-05-04 17:18:10 +0200

    Fix errors

--- backup/data.sh
+++ backup/data.sh
@@ -5,14 +5,13 @@
 echo "#### Restore Data"
 # Remove all none hidden folders from home
 echo "Removing home folders..."
-cd ~
-rm -rf -- */
+rm -rf -- ~/*/
 
 # Restore home folder without dotfiles
 echo "Restore data from backup..."
 for i in $BACKUP/*; do
 	echo "Restoring $i"
-	$copy --info=progress2 $i ./
+	$copy --info=progress2 $i ~
 done
 
 echo -e "\n"
--- lightdm/lightdm.sh
+++ lightdm/lightdm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Install LightDM theme"
-$aurinst llightdm-webkit-theme-litarvan >> log
+$aurinst lightdm-webkit-theme-litarvan >> $log
 
 sudo rm /etc/lightdm/lightdm-webkit2-greeter.conf
 sudo -E cp lightdm/lightdm-webkit2-greeter.conf /etc/lightdm/
