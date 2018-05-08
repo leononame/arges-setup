157c80382e5fca31368069a24188bbe819ce771d
Leo <leononame@lho.io>
2018-05-08 22:58:18 +0200

    Disable automount, add lightdm

---
+++ lightdm/lightdm.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Install LightDM theme"
+$aurinst llightdm-webkit-theme-litarvan >> log
--- settings.sh
+++ settings.sh
@@ -15,3 +15,11 @@ sudo locale-gen
 dconf write /org/gnome/desktop/input-sources/sources "[('xkb', 'us'), ('xkb', 'de'), ('xkb', 'es')]"
 # Date/Time and number format
 dconf write /system/locale/region "'es_ES.UTF-8'"
+
+
+echo "Disable automount"
+sudo mkdir -p /etc/dconf/profile
+sudo mkdir -p /etc/dconf/db/local.d
+sudo -E cp files/user /etc/dconf/profile/
+sudo -E cp files/00-disable-automount /etc/dconf/db/local.d/
+
