36eb411987e6b929c303cd569afd50d3a5d088e7
Leo <leoreentry@lho.io>
2018-06-15 10:56:10 +0200

    implement disabling gnome-tracker

--- settings.sh
+++ settings.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Disable tracker
+tracker daemon -t
+cd ~/.config/autostart
+cp -v /etc/xdg/autostart/tracker-* ./
+for FILE in `ls tracker-*`; do echo Hidden=true >> $FILE; done
+rm -rf ~/.cache/tracker ~/.local/share/tracker
+
 # Locale
 
 # Backup locale
