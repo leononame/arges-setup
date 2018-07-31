2298fc58714ee84201eb7632acdb422cf393b35c
Leo <leononame@lho.io>
2018-07-31 13:00:53 +0200

    Fix some package stuff

--- packages/install.sh
+++ packages/install.sh
@@ -2,11 +2,14 @@
 
 # Install pacman packages
 echo "Official packages"
+sudo pacman -Syyu
 sudo -E $inst - < packages/pkglist >>$log
 
 # Install aurman
 echo "aurman"
 packages/aurman.sh >>$log
+# Show message confirmation window
+aurman -Syyu
 
 # Install zprezto
 echo "zprezto"
