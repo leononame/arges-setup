57c9655c3487bc0fd90af8c5397f6868dbbc89fa
Leo <leononame@lho.io>
2018-08-09 12:55:50 +0200

    Add new theme for lightdm

--- lightdm/lightdm.sh
+++ lightdm/lightdm.sh
@@ -2,6 +2,7 @@
 
 echo "Install LightDM theme"
 $aurinst lightdm-webkit-theme-litarvan >> $log
+$aurinst lightdm-webkit-theme-material2 >> $log
 
 sudo rm /etc/lightdm/lightdm-webkit2-greeter.conf
 sudo -E cp lightdm/lightdm-webkit2-greeter.conf /etc/lightdm/
